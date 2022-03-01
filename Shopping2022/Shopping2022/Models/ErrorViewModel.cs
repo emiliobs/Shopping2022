e5dd58792b1a6db4f4b00af37e28131a82952ee5
emilio barrera <barrera_emilio@yahoo.es>
2022-03-01 05:12:09 +0000

    Created application

---
+++ Shopping2022/Shopping2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopping2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
