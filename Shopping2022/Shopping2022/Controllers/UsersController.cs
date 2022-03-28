892632c26492bd4cd5e1cbe77a932be8088bd56a
emilio barrera <barrera_emilio@yahoo.es>
2022-03-28 19:06:59 +0100

    Already Created Admintraitor in UsersController and View Create and more

--- Shopping2022/Shopping2022/Controllers/UsersController.cs
+++ Shopping2022/Shopping2022/Controllers/UsersController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shopping2022.Data;
+using Shopping2022.Data.Entities;
+using Shopping2022.Enums;
+using Shopping2022.Helpers;
+using Shopping2022.Models;
 
 namespace Shopping2022.Controllers
 {
@@ -9,18 +13,104 @@ namespace Shopping2022.Controllers
     public class UsersController : Controller
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public UsersController(DataContext context)
+        public UsersController(DataContext context, IUserHelper userHelper, ICombosHelper combosHelper, IBlobHelper blobHelper)
         {
-            this._context = context;
+            _context = context;
+            _userHelper = userHelper;
+            _combosHelper = combosHelper;
+            _blobHelper = blobHelper;
         }
 
 
 
-        public async Task<IActionResult> Index() => View(await _context.Users
-                                                               .Include(u => u.City)
-                                                               .ThenInclude(c => c.State)
-                                                               .ThenInclude(s => s.Country)
-                                                               .ToListAsync());
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users
+                         .Include(u => u.City)
+                         .ThenInclude(c => c.State)
+                         .ThenInclude(s => s.Country)
+                         .ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            AddUserViewModel addUserViewModel = new AddUserViewModel
+            {
+                Id = Guid.Empty.ToString(),
+                Countries = await _combosHelper.GetComboCountriesAsync(),
+                States = await _combosHelper.GetComboStatesByIdAsync(0),
+                Cities = await _combosHelper.GetComboCitiesByIdyAsync(0),
+                UserType = UserType.Admin,
+            };
+
+            return View(addUserViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (addUserViewModel.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(addUserViewModel.ImageFile, "users");
+                }
+
+                addUserViewModel.ImageId = imageId;
+                Data.Entities.User? user = await _userHelper.AddUserAsync(addUserViewModel);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo Usado.");
+                    addUserViewModel.Countries = await _combosHelper.GetComboCountriesAsync();
+                    addUserViewModel.States = await _combosHelper.GetComboStatesByIdAsync(addUserViewModel.StateId);
+                    addUserViewModel.Cities = await _combosHelper.GetComboCitiesByIdyAsync(addUserViewModel.CityId);
+
+                    return View(addUserViewModel);
+                }
+
+                return RedirectToAction("Index", "Users");
+
+            }
+
+            addUserViewModel.Countries = await _combosHelper.GetComboCountriesAsync();
+            addUserViewModel.States = await _combosHelper.GetComboStatesByIdAsync(addUserViewModel.StateId);
+            addUserViewModel.Cities = await _combosHelper.GetComboCitiesByIdyAsync(addUserViewModel.CityId);
+
+            return View(addUserViewModel);
+        }
+
+        public JsonResult GetStates(int countryId)
+        {
+            Country country = _context.Countries
+                          .Include(c => c.States)
+                          .FirstOrDefault(c => c.Id == countryId);
+            if (country == null)
+            {
+                return null;
+            }
+
+            return Json(country.States.OrderBy(s => s.Name));
+
+        }
+
+        public JsonResult GetCities(int stateId)
+        {
+            State state = _context.States.Include(s => s.Cities).FirstOrDefault(s => s.Id == stateId);
+            if (state == null)
+            {
+                return null;
+            }
+
+            return Json(state.Cities.OrderBy(s => s.Name));
+
+        }
     }
 }
