a006fe3d7aef507c4f558af888451a3173d55cb8
emilio barrera <barrera_emilio@yahoo.es>
2022-04-09 18:04:49 +0100

    Edit products already

--- Shopping2022/Shopping2022/Controllers/ProductsController.cs
+++ Shopping2022/Shopping2022/Controllers/ProductsController.cs
@@ -48,33 +48,33 @@ namespace Shopping2022.Controllers
         {
             if (ModelState.IsValid)
             {
-                var imageId = Guid.Empty;
+                Guid imageId = Guid.Empty;
                 if (model.ImageFile != null)
                 {
                     imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "products");
 
                 }
 
-                Product product = new() 
+                Product product = new()
                 {
                     Description = model.Description,
                     Name = model.Name,
                     Price = model.Price,
                     Stock = model.Stock,
-                };
 
-                //<aqui le agrego al menos una categoria al producto nuevo por 1 ves.
-                product.ProductCategories = new List<ProductCategory> 
-                { 
+                    //<aqui le agrego al menos una categoria al producto nuevo por 1 ves.
+                    ProductCategories = new List<ProductCategory>
+                {
                     new ProductCategory
                     {
                         Category = await _context.Categories.FindAsync(model.CategoryId),
                     }
-                 };
+                 }
+                };
 
                 if (imageId != Guid.Empty)
                 {
-                    product.ProductImages = new List<ProductImage> 
+                    product.ProductImages = new List<ProductImage>
                     {
                       new ProductImage { ImageId = imageId}
                     };
@@ -97,7 +97,7 @@ namespace Shopping2022.Controllers
                         ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                     }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     ModelState.AddModelError(string.Empty, ex.Message);
                 }
@@ -107,5 +107,77 @@ namespace Shopping2022.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products.FindAsync(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            EditProductViewModel model = new EditProductViewModel
+            {
+                Description = product.Description,
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CreateProductViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+
+                try
+                {
+                    Product product = await _context.Products.FindAsync(model.Id);
+
+                    product.Description = model.Description;
+                    product.Name = model.Name;
+                    product.Price = model.Price;
+                    product.Stock = model.Stock;
+
+                    _context.UpdateRange(product);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            
+
+            return View(model);
+        }
     }
 }
