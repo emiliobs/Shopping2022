c816005002c4688c1fdacc1337efac4108436f1b
emilio barrera <barrera_emilio@yahoo.es>
2022-04-24 15:27:25 +0100

    Basic home screem

--- Shopping2022/Shopping2022/Controllers/HomeController.cs
+++ Shopping2022/Shopping2022/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping2022.Data;
+using Shopping2022.Data.Entities;
 using Shopping2022.Models;
 using System.Diagnostics;
 
@@ -7,15 +10,53 @@ namespace Shopping2022.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product> products = await _context.Products.Include(p => p.ProductImages)
+                                                  .Include(p => p.ProductCategories)
+                                                  .OrderBy(p => p.Description).ToListAsync();
+
+            List<ProductHomeViewModel> productHome = new List<ProductHomeViewModel>() { new ProductHomeViewModel()};
+            
+            int i = 1;
+            foreach (Product product in products)
+            {
+                if (i == 1)
+                {
+                    productHome.LastOrDefault().Product1 = product;
+                }
+
+                if (i == 2)
+                {
+                    productHome.LastOrDefault().Product2 = product;
+                }
+
+                if (i == 3)
+                {
+                    productHome.LastOrDefault().Product3 = product;
+                }
+
+                if (i == 4)
+                {
+                    productHome.LastOrDefault().Product4 = product;
+                    productHome.Add(new ProductHomeViewModel());
+
+                    i = 0;
+                }
+
+                i++;
+            }
+
+
+            return View(productHome);
         }
 
         public IActionResult Privacy()
---
+++ Shopping2022/Shopping2022/Models/ProductHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Shopping2022.Data.Entities;
+
+namespace Shopping2022.Models
+{
+    public class ProductHomeViewModel
+    {
+        public Product Product1 { get; set; }
+
+        public Product Product2 { get; set; }
+
+        public Product Product3 { get; set; }
+
+        public Product Product4 { get; set; }
+
+    }
+}
